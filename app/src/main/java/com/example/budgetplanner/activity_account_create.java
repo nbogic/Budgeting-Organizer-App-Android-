23f5b0840188769ff0eacea35b723912b8b82f9c
Nemanja <38273606+nbogic@users.noreply.github.com>
2019-04-04 18:42:38 +1100

    Project files

---
+++ app/src/main/java/com/example/budgetplanner/activity_account_create.java
@@ -0,0 +1,13 @@
+package com.example.budgetplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_account_create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_account_create);
+    }
+}
